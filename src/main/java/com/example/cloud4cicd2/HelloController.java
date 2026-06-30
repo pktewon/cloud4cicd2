9ec62008daec4ecf37ca0640433fd5e197654b8d
김태준 <taejun040330@gamil.com>
2026-06-30 11:33:12 +0900

    github actions 빌드하고 jar 복사하기 cicd

--- src/main/java/com/example/cloud4cicd2/HelloController.java
+++ src/main/java/com/example/cloud4cicd2/HelloController.java
@@ -11,4 +11,10 @@ public class HelloController {
     public String hi(){
         return "hi cicd2 github action ^&&&^";
     }
+
+    @GetMapping("/hi2")
+    public String hi2(){
+        return "hi 태준";
+    }
+
 }
